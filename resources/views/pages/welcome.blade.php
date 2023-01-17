9f7522781f5a5ce927274de99ac5f21123fac674
Gianluca Sirianni <gianluca.sirianni7@gmail.com>
2023-01-17 14:24:58 +0100

    Esercizio Completato

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -3,5 +3,30 @@
 
 
 @section('main-content')
-<h1>ciao</h1>
+
+
+            <div class="container">
+                <table class="table text-center">
+                    <thead>
+                        <tr>
+                            <th scope="col">Numero treno</th>
+                            <th scope="col">Destinazione</th>
+                            <th scope="col">Data e orario di partenza</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($trains as $elem)
+                        <tr>
+
+                            <td>{{ $elem->train_number }}</td>
+                            <td>{{ $elem->destination }}</td>
+                            <td>{{ $elem->departure_time }}</td>
+                        </tr>
+                    </tbody>
+                    @endforeach
+                </table>
+            </div>
+
+
+
 @endsection
